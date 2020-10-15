29866a97f2a3e4db72e03d1105c636296240b38f
PayThePro <paythepro@gmail.com>
2020-10-15 22:15:28 +0200

    The project

---
+++ Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace AndengradslommeregnerV2
+{
+    partial class Andengradslommeregner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Andengradslommeregner));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.AccessibleName = "";
+            this.textBox1.Font = new System.Drawing.Font("Segoe Print", 21.75F, System.Drawing.FontStyle.Bold);
+            this.textBox1.Location = new System.Drawing.Point(125, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 59);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 337);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 51);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsReturn = true;
+            this.textBox2.AcceptsTab = true;
+            this.textBox2.AccessibleName = "";
+            this.textBox2.Font = new System.Drawing.Font("Segoe Print", 21.75F, System.Drawing.FontStyle.Bold);
+            this.textBox2.Location = new System.Drawing.Point(125, 77);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(185, 59);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.AcceptsReturn = true;
+            this.textBox3.AcceptsTab = true;
+            this.textBox3.AccessibleName = "";
+            this.textBox3.Font = new System.Drawing.Font("Segoe Print", 21.75F, System.Drawing.FontStyle.Bold);
+            this.textBox3.Location = new System.Drawing.Point(125, 142);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(185, 59);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // Button1
+            // 
+            this.Button1.Font = new System.Drawing.Font("Segoe Print", 21.75F, System.Drawing.FontStyle.Bold);
+            this.Button1.Location = new System.Drawing.Point(125, 207);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(185, 59);
+            this.Button1.TabIndex = 4;
+            this.Button1.Text = "Calculate";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 21.75F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(71, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 51);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "a:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe Print", 21.75F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(71, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 51);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "b:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe Print", 21.75F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(73, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 51);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "c:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe Print", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 439);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 51);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "0";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe Print", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 541);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(121, 51);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "(0 ; 0)";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe Print", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 286);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(239, 51);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Discriminant =";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe Print", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(12, 388);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(173, 51);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Answers =";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe Print", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(12, 490);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(194, 51);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Top point =";
+            // 
+            // Andengradslommeregner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(434, 611);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Andengradslommeregner";
+            this.Text = "Andengradslommeregner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AndengradslommeregnerV2
+{
+    public partial class Andengradslommeregner : Form
+    {
+        private double Discriminant;
+        private double NegativeDiscriminant;
+        private double SquarerootOfDiscriminant;
+
+        private double Answer1;
+        private double Answer2;
+
+        private double Tx;
+        private double Ty;
+        
+        private string STRINGa;
+        private string STRINGb;
+        private string STRINGc;
+
+        private double a;
+        private double b;
+        private double c;
+
+
+        public Andengradslommeregner()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Convets all string values to floats
+
+            //a
+            STRINGa = textBox1.Text;
+            a = System.Convert.ToSingle(STRINGa);
+            //b
+            STRINGb = textBox2.Text;
+            b = System.Convert.ToSingle(STRINGb);
+            //c
+            STRINGc = textBox3.Text;
+            c = System.Convert.ToSingle(STRINGc);
+
+            //Calculates Discriminant
+            Discriminant = ((b * b) - (4 * a * c)); ;
+            NegativeDiscriminant = ((Discriminant - Discriminant - Discriminant));
+            SquarerootOfDiscriminant = Math.Sqrt(Discriminant);
+
+            //Calculates Answers
+            if (Discriminant >= 0)
+                Answer1 = (-b - Math.Sqrt(Discriminant)) / (2 * a);
+
+            if (Discriminant > 0)
+                Answer2 = ((-b + Math.Sqrt(Discriminant))) / (2 * a);
+
+            //Calculates Top Points
+            Tx = -b / (2 * a);
+            Ty = -Discriminant / (4 * a);
+
+            //Rounds Decimals
+            Discriminant = Math.Round(Discriminant, 5);
+            Answer1 = Math.Round(Answer1, 5);
+            Answer2 = Math.Round(Answer2, 5);
+            Tx = Math.Round(Tx, 5);
+            Ty = Math.Round(Ty, 5);
+
+            //Draws answers
+            label1.Text = "" + Discriminant;
+
+            if (Discriminant > 0)
+            {
+                label5.Text = Answer1 + " and " + Answer2;
+            }
+            else if (Discriminant == 0)
+            {
+                label5.Text = "" + Answer1;
+            }
+            else if (Discriminant < 0)
+            {
+                label5.Text = "none";
+            }
+
+            label6.Text = "(" + Tx + " ; " + Ty + ")";
+
+            //Resets Answers
+            Answer1 = 0;
+            Answer2 = 0;
+            Tx = 0;
+            Ty = 0;
+        }
+    }
+}
